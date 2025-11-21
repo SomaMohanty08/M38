78924c5489525e3a33c0e8c1cd8e3b7167c3ce45
ASUS <ASUS@DESKTOP-J8LE05M>
2025-11-21 11:31:41 +0530

    commit

---
+++ M38/src/main/java/M38/M38/App.java
@@ -0,0 +1,13 @@
+package M38.M38;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ M38/src/test/java/DEMO/Dummy.java
@@ -0,0 +1,11 @@
+package DEMO;
+
+public class Dummy {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello students");	
+		
+	}
+
+}
